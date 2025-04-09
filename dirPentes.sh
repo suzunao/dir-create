2abf79b281a648214ae8e081223a51a33d336aa7
suzunao <suzun4o@outlook.com>
2025-04-09 03:06:06 -0500

    update 0.1

---
+++ dirPentes.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+show_help() {
+    echo "Uso: $0 <nombre_del_directorio> <-htb | -thm>"
+    echo "  -h       Mostrar este mensaje de ayuda"
+    echo "  -htb     Crear los directorios en la ruta HTB (/\$home/Documents/HTB/)"
+    echo "  -thm     Crear los directorios en la ruta THM (/\$home/Documents/THM/)"
+}
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Parametro no proporcionado."
+    exit 1
+fi
+
+if [ "$1" == "-h" ] || [ "$2" == "-h" ]; then
+    show_help
+    exit 0 
+fi 
+
+dirnombre=$2
+
+case "$1" in
+    -htb) ruta="$HOME/Documents/HTB/" ;;
+    -thm) ruta="$HOME/Documents/THM/" ;;
+    *) echo "Parámetro inválido: usa -htb o -thm"; exit 1 ;;
+esac
+
+[ ! -d "$ruta" ] && mkdir -p "$ruta"
+
+ruta_principal="$ruta$dirnombre"
+mkdir -p "$ruta_principal"
+
+mkdir -p "$ruta_principal"/{scan,enumeration/{web,smb,ftp},exploit,evidence/screenshots,scripts,tools}
+
+if command -v tree &>/dev/null; then
+    tree "$ruta_principal"
+else
+    echo "Directorio creado en: $ruta_principal"
+    echo "Instala 'tree' para ver la estructura: sudo apt install tree"
+fi
+
+
+
+
+
